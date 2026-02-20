845c1ce4389e017f74d7ae7262061385a8aa1d23
Brooke Cchen <brookecchen@Mac.fios-router.home>
2026-02-20 14:08:19 -0500

    fix: simplify index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const ENV_VARS = ['LOG_LEVEL', 'API_URL', 'FEATURE_FLAG', 'DB_PASSWORD', 'STRIPE_KEY'];
+
+const server = http.createServer((req, res) => {
+  const results = {};
+  ENV_VARS.forEach(key => {
+    const val = process.env[key];
+    results[key] = val ? 'SET (' + val.slice(0, 4) + '****)' : 'NOT SET';
+  });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(JSON.stringify(results, null, 2));
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
